aee89d372a3eec40f5abd10d5beecbda4f907e45
Tobias Straub <hello@tobiasstraub.com>
2018-10-12 09:47:03 +0200

    Basic source code

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Dependency of express framework
+const express = require("express");
+
+// Initialize express framework
+const app = express();
+
+// Listening port
+const port = 3050;
+
+// Route and content definition
+app.get("/", (req, res) => res.send("Hello World!"));
+
+// Starting server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
